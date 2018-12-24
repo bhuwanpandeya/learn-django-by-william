3a8fb4ebf2738ba56286d6df1a59c948e8c178ee
bhuwanpandeya <bpandeya70@gmail.com>
2018-12-24 12:26:52 +0545

    initial commit

--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'homepage',
     'posts',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
